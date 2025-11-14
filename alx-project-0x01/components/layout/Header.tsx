6fb8b30685b84951ba491f05cbbf22eeb337b4e7
Usytech01 <usmandioo@yahoo.com>
2025-11-14 15:43:44 +0100

    add task 1

---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Header: React.FC = () => {
+    return(
+        <header className="bg-blue-600 text-white shadow-md py-4">
+            <div className="container mx-auto flex justify-between items-center px-4">
+                <h3><Link href="/">Daily Contents</Link></h3>
+                <nav>
+                    <ul className="flex space-x-6">
+                        <li className="hover:underline"><Link href="/">Posts</Link></li>
+                        <li className="hover:underline"><Link href="/">Users</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,11 +1,22 @@
-
+import Header from "@/components/layout/Header";
 
 const Home: React.FC = () => {
-  return (
-    <div className="flex justify-center itmes-center h-screen">
-      <h1 className="text-7x1 font-thin">Welcome Page</h1>
-    </div>
-  )
+    return(
+        <div className="flex flex-col h-screen">
+          {/* header from component */}
+          <Header /> 
+
+          {/* main body */}
+          <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+            <div>
+              <h1 className="text-5xl font-bold">Welcom to our Application</h1>
+              <p className="mt-4 text-white text-xl">We're glad you're here. Explore and enjoy your experience.</p>
+              <button className="mt-6 px-6 py-2 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 transition">Get Started</button>
+            </div>
+          </main>
+
+        </div>
+    )
 }
 
-export default Home;
+export default Home
