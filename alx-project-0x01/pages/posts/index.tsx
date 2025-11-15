405b66389003eac8f727dd7b3bce907f1cf81431
Usytech01 <usmandioo@yahoo.com>
2025-11-15 02:31:51 +0100

    users index

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/layout/Header";
+import React from 'react'
+
+export default function index() {
+  return (
+    
+    <Header/>
+  )
+}
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/layout/Header";
+
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div><Header/ ></div>
+  )
+}
