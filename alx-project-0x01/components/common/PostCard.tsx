6eb86af04f64d1a50fdb3cc734590da967554aa1
Usytech01 <usmandioo@yahoo.com>
2025-11-15 02:11:07 +0100

    first commit

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,9 @@
+import { PostProps } from "@/interfaces";
+
+const PostCard: React.FC <PostProps> = ({ title, body, userID, id }) => {
+    return(
+        <div></div>
+    )
+}
+
+export default PostCard;
