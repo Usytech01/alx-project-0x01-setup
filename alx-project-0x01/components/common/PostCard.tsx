5d619b4ac59d910a53cbdfefa8499188ffe2f121
Usytech01 <usmandioo@yahoo.com>
2025-11-15 15:06:39 +0100

    task 2

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,9 +1,18 @@
 import { PostProps } from "@/interfaces";
 
-const PostCard: React.FC <PostProps> = ({ title, body, userID, id }) => {
+const PostCard: React.FC <PostProps> = ({ title, body, userId, id }) => {
     return(
-        <div></div>
-    )
+        <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shodow duration-300">
+            <div>
+                <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>
+            </div>
+            <p className="text-2xl font-semibold text-gray-600">{body}</p>
+            <div>
+                <span>User ID: {userId}</span>
+                <span>Post ID: {id}</span>
+            </div>
+        </div>
+    );
 }
 
 export default PostCard;
--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,37 @@
 import Header from "@/components/layout/Header";
-import React from 'react'
+import PostCard from "@/components/common/PostCard";
+import { PostProps } from "@/interfaces";
 
-export default function index() {
-  return (
-    
-    <Header/>
-  )
+const Post: React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="flex justify-between">
+                    <h1 className="text-2x1 font-semibold">Post Content</h1>
+                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+                </div>
+                    <div className="grid grid-cols-3 gap-2">
+                        {
+                            Post?.map(({ title, body, userId, id}: PostProps, key: number) => (
+                                <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+                                
+                            ))
+                        }
+                    </div>
+            </main>
+        </div>
+    )
 }
+
+export async function getStaticProps() {
+    const response = await fetch ("https://jsonplaceholder.typicode.com/posts")
+    const posts = await response.json()
+    return{
+        Props:{
+            posts
+        }
+    }
+}
+
+export default Post;
